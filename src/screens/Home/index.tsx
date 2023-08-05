678933213887d4e1791fe0be0d3177f84ac0730f
Guilherme Teixeira <guilherme.augusto23@hotmail.com>
2023-08-05 14:38:14 -0300

    feat: adding and removing participants using react state management

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,54 +8,50 @@ import {
 } from "react-native";
 import { styles } from "./styles";
 import Participant from "../../components/Participant";
+import { useState } from "react";
 
 export default function Home() {
-  const participants = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
+
+
   const handleParticipantAdd = () => {
-    if (participants.includes("a")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante já existe",
         "Já existe um participante na lista com esse nome"
       );
     }
+
+    setParticipants([...participants, participantName]);
+    setParticipantName('')
   };
 
   const handleParticipantRemove = (name: string) => {
     Alert.alert('Remover', 'Remover o participante ' + name + '?', [
       {
         text: 'Sim',
-        onPress: () =>Alert.alert (`${name} deletado!`)
+        onPress: () =>{
+          setParticipants(participants.filter(participant => participant != name))
+        }
       }, 
       {
         text: 'Não'
       }
     ])
-
-    console.log("Removeu ", name);
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Casa do menino Ney</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sabádo 18 de novembro</Text>
       <View style={styles.form}>
         <TextInput
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6b6b6b"
+          value={participantName}
+          onChangeText={setParticipantName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
